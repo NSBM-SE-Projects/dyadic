fc7d921929280887ca1dec89e26904a74b61cfc5
dwainXDL <personal.dwain@gmail.com>
2026-04-17 23:02:37 +0530

    test(unit): add domain entity and RoleSeeder tests

---
+++ tests/Dyadic.UnitTests/Domain/ProposalTests.cs
@@ -0,0 +1,40 @@
+using Dyadic.Domain.Entities;
+using Dyadic.Domain.Enums;
+using FluentAssertions;
+
+namespace Dyadic.UnitTests.Domain;
+
+public class ProposalTests {
+    [Fact]
+    public void Status_ShouldDefaultToDraft() {
+        var proposal = new Proposal {
+            Title = "Test Proposal",
+            Description = "A test"
+        };
+
+        proposal.Status.Should().Be(ProposalStatus.Draft);
+    }
+
+    [Fact]
+    public void CreatedAt_ShouldDefaultToUtcNow() {
+        var before = DateTime.UtcNow;
+        var proposal = new Proposal {
+            Title = "Test",
+            Description = "Test"
+        };
+        var after = DateTime.UtcNow;
+
+        proposal.CreatedAt.Should().BeOnOrAfter(before).And.BeOnOrBefore(after);
+    }
+
+    [Fact]
+    public void SupervisorId_ShouldBeNullByDefault() {
+        var proposal = new Proposal {
+            Title = "Test",
+            Description = "Test"
+        };
+
+        proposal.SupervisorId.Should().BeNull();
+        proposal.Supervisor.Should().BeNull();
+    }
+}
