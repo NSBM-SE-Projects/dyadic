2b556148135b19fcf31b0478c6d9ce6b39d22cd4
Thamindu Mihisara <thamindumihisara836@gmail.com>
2026-04-18 15:17:36 +0530

    chore: restore standard ASP.NET section comments in Program.cs

--- src/Dyadic.Web/Program.cs
+++ src/Dyadic.Web/Program.cs
@@ -8,6 +8,7 @@ using Dyadic.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -32,6 +33,7 @@ builder.Services.AddScoped<IProposalService, ProposalService>();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 using (var scope = app.Services.CreateScope()) {
     await DataSeeder.SeedAsync(scope.ServiceProvider);
 }
@@ -39,6 +41,7 @@ using (var scope = app.Services.CreateScope()) {
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
