11e972298877da6a7b2f9e22db58dca5fcfed024
Thamindu Mihisara <thamindumihisara836@gmail.com>
2026-04-18 14:47:27 +0530

    fix: remove duplicate nav links and add [Authorize] to Dashboard

--- src/Dyadic.Web/Pages/Dashboard.cshtml.cs
+++ src/Dyadic.Web/Pages/Dashboard.cshtml.cs
@@ -1,7 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Dyadic.Web.Pages;
-
+[Authorize]
 public class DashboardModel : PageModel
 {
     public void OnGet() { }
